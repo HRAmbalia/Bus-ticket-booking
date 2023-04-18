956c31317a67715ab2ceb319821cb02d7f2ab75d
HRAmbalia <ambaliaharshit25@gmail.com>
2023-04-18 21:17:24 +0530

    Updated settings

--- BTB/settings.py
+++ BTB/settings.py
@@ -14,6 +14,7 @@ from pathlib import Path
 import os
 #
 from django.contrib.messages import constants as messages
+from config import SECRET_KEY
 #
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
@@ -24,7 +25,7 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 # See https://docs.djangoproject.com/en/3.1/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = '!az41qkf(6e@b8=p!*8$ru0kl%o&o-e59&@tj-+ltj(65u2fn+'
+SECRET_KEY = SECRET_KEY
 
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
